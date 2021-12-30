511fb89f5d8687d3c5d407f87f86d92f47ad1703
Sefa Yıldırım <45906230+sefayildirim@users.noreply.github.com>
2021-12-31 01:35:29 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,91 @@
+(function ($) {
+  "use strict";
+
+	
+ $(window).scroll(function () {
+    var window_top = $(window).scrollTop() + 1;
+    if (window_top > 50) {
+      $('.main_menu').addClass('menu_fixed animated fadeInDown');
+    } else {
+      $('.main_menu').removeClass('menu_fixed animated fadeInDown');
+    }
+  });
+ 
+$('.testimonial-slider').slick({
+		slidesToShow: 1,
+		infinite: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		vertical:true,
+		verticalSwiping:true
+	});
+
+ 
+
+
+
+	var map;
+
+	function initialize() {
+		var mapOptions = {
+			zoom: 13,
+			center: new google.maps.LatLng(50.97797382271958, -114.107718560791)
+			// styles: style_array_here
+		};
+		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	}
+
+	var google_map_canvas = $('#map-canvas');
+
+	if (google_map_canvas.length) {
+		google.maps.event.addDomListener(window, 'load', initialize);
+	}
+
+	// Counter
+
+	$('.count').counterUp({
+	      delay: 10,
+	      time: 1000
+	  });
+
+
+
+	//    Contact 
+	var form = $('.contact_form'),
+        message = $('.contact__msg'),
+        form_data;
+
+    // Success function
+    function done_func(response) {
+        message.fadeIn().removeClass('alert-danger').addClass('alert-success');
+        message.text(response);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 2000);
+        form.find('input:not([type="submit"]), textarea').val('');
+    }
+
+    // fail function
+    function fail_func(data) {
+        message.fadeIn().removeClass('alert-success').addClass('alert-success');
+        message.text(data.responseText);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 2000);
+    }
+    
+    form.submit(function (e) {
+        e.preventDefault();
+        form_data = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: form.attr('action'),
+            data: form_data
+        })
+        .done(done_func)
+        .fail(fail_func);
+    });
+	
+
+}(jQuery));
